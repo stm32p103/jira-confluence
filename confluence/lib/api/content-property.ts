09f2691340558ccb6298a43c0a085753de7cf8ee
stm32p103 <stm32p103@gmail.com>
2019-08-31 18:41:17 +0900

    Typescript deep diveを参考に、null->undefinedに置き換え

--- confluence/lib/api/content-property.ts
+++ confluence/lib/api/content-property.ts
@@ -1,75 +1,89 @@
 import { Range } from './range';
 import { RestAPI, Request, queryToString } from '../request';
 
+/* ------------------------------------------------------------------------
+ * Content Property
+ * --------------------------------------------------------------------- */
 export interface ContentProperty {
-    cid: string;
-    id: string;
-    key: string;
-    value: any;
-    version: {
-        when: Date;
-        number: number;
-    }
+  cid: string;
+  id: string;
+  key: string;
+  value: any;
+  version: {
+    when: Date;
+    number: number;
+  }
 }
 
+/* ------------------------------------------------------------------------
+ * Content Property List
+ * 複数取得する際の型
+ * --------------------------------------------------------------------- */
 export interface ContentPropertyList {
-    results: ContentProperty[];
-    range: Range;
+  results: ContentProperty[];
+  range: Range;
 }
 
+/* ------------------------------------------------------------------------
+ * Functions
+ * --------------------------------------------------------------------- */
 function apiUrl( cid: string, key?: string ) {
-    let url = [ '/rest/api/content', cid, 'property' ].join( '/' );
-    
-    if( key ) {
-        url = url + '/' + key;
-    }
-    
-    return url;
+  let url = [ '/rest/api/content', cid, 'property' ].join( '/' );
+  if( key ) {
+    url = url + '/' + key;
+  }
+  return url;
 }
 
 /* ------------------------------------------------------------------------
  * 受け取ったデータを整形する
  * --------------------------------------------------------------------- */
-function format( cid: string, result: any ): ContentProperty {
-    let tmp = null;
-    if( result !== null ) {
-        tmp = {
-            cid: cid,
-            id: result.id,
-            key: result.key,
-            value: result.value,
-            version: {
-                when: result.version.when,
-                number: result.version.number
-            }
-        };
-    }
-    return tmp;
+function format( cid: string, result?: any ): ContentProperty {
+  if( result ) {
+    return {
+      cid: cid,
+      id: result.id,
+      key: result.key,
+      value: result.value,
+      version: {
+        when: result.version.when,
+        number: result.version.number
+      }
+    };
+  } else {
+    return undefined;
+  }
+}
+
+/* ------------------------------------------------------------------------
+ * 複数データを返す応答からRangeを取得する
+ * --------------------------------------------------------------------- */
+function rangeFromResponse( res: any ): Range {
+  return new Range( {
+    start: res.start,
+    limit: res.limit,
+    size: res.size
+  } );
 }
 
 export class ContentPropertyAPI {
-    /* ------------------------------------------------------------------------
-     * Dependency Injection
-     * --------------------------------------------------------------------- */
-    constructor( private readonly api: RestAPI ) {}
+  constructor( private readonly api: RestAPI ) {}
     
-	/* ------------------------------------------------------------------------
-	 * 全てのプロパティを取得する(REST API)
+  /* ------------------------------------------------------------------------
+   * 全てのプロパティを取得する(REST API)
 	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-findAll
 	 * --------------------------------------------------------------------- */
 	private async restFindAll( cid: string, param: any = {} ) {
-		let response;
 		const req = Request.get( apiUrl( cid ) + queryToString( param ) );
 		try {
-		    response = await this.api.request( req );					
+		  return await this.api.request( req );
 		} catch( err ) {
 			if( err.status === 404 ) {
-				response = null;
+			  return undefined;
 			} else {
-				throw new Error( err );
+			  throw new Error( err );
 			}
 		}
-		return response;
 	}
 
 	/* ------------------------------------------------------------------------
@@ -77,123 +91,122 @@ export class ContentPropertyAPI {
 	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-findByKey
 	 * --------------------------------------------------------------------- */
 	private async restFindByKey( cid: string, key: string, param: any = {} ) {
-        let response;
-        const req = Request.get( apiUrl( cid, key ) + queryToString( param ) );
-        
-        try { 
-            response = await this.api.request( req );
-        } catch( err ) {
-            if( err.status === 404 ) {
-                response = null;
-            } else {
-                throw new Error( err );
-            }
-        }
-        
-        return response;
+	  const req = Request.get( apiUrl( cid, key ) + queryToString( param ) );
+	  try { 
+	    return await this.api.request( req );
+	  } catch( err ) {
+	    if( err.status === 404 ) {
+	      return undefined;
+	    } else {
+	      throw new Error( err );
+	    }
+	  }
 	}
 	
 	/* ------------------------------------------------------------------------
-     * key を value で新規作成する。
-     * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-create
-     * --------------------------------------------------------------------- */
+	 * key を value で新規作成する。
+	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-create
+	 * --------------------------------------------------------------------- */
 	private async restCreate( cid: string, key: string, value: any ) {
-        const req = Request.post( apiUrl( cid ) );
-        req.attatchData( { key: key, value: value } );
-
-        let response;
-        try {
-            response = await this.api.request( req );            
-        } catch ( err ) {
-            console.log( err );
-        }
-        return response;
-    }
-    /* ------------------------------------------------------------------------
-     * key を value で更新する。バージョンは数字で指定する。(過去と同じ値だとエラー)
-     * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-update
-     * --------------------------------------------------------------------- */
+	  const req = Request.post( apiUrl( cid ) );
+	  req.attatchData( { key: key, value: value } );
+	  try {
+	    return await this.api.request( req );            
+	  } catch ( err ) {
+	    // TODO: handle 400, 403, 413, 415 error
+	    throw new Error( err );
+	  }
+	}
+	/* ------------------------------------------------------------------------
+	 * key を value で更新する。バージョンは数字で指定する。(過去と同じ値だとエラー)
+	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-update
+	 * --------------------------------------------------------------------- */
 	private async restUpdate( cid: string, key: string, value: any, version: number ) {
-        const req = Request.put( apiUrl( cid, key ) );
-        req.attatchData( { key: key, value: value, version: { number: version } } );
-        let response = await this.api.request( req );
-        return response;
-    }
-    /* ------------------------------------------------------------------------
-     * keyを削除。
-     * 404 NOT FOUND はエラーとみなさない。
-     * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-delete
-     * --------------------------------------------------------------------- */
+	  const req = Request.put( apiUrl( cid, key ) );
+	  req.attatchData( { key: key, value: value, version: { number: version } } );
+	  
+	  try {
+	    return await this.api.request( req );
+	  } catch( err ) {
+	    throw new Error( err );
+	  }
+	}
+	/* ------------------------------------------------------------------------
+	 * keyを削除。
+	 * 404 NOT FOUND はエラーとみなさない。
+	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content/{id}/property-delete
+	 * --------------------------------------------------------------------- */
 	private async restDelete( cid: string, key: string ) {
-        const req = Request.delete( apiUrl( cid, key ) );
-        try {
-            let response = await this.api.request( req );
-            await this.api.request( req );
-        } catch( err ) {            
-            if( err.status  == 404 ) {
-                throw new Error( err );
-            }
-        }
-    }
-    /* ------------------------------------------------------------------------
-     * 検索
-     * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content-search
-     * 更新日把握のため versionも取得する。もう少し拡張性のある書き方にしたい。
-     * --------------------------------------------------------------------- */
-    private async restSearch( param: any = {} )  {
-        let response = null;
-        const url = '/rest/api/content/search';
-        const req = Request.get( url + queryToString( param ) );
-        try {
-            response = await this.api.request( req );
-        } catch( err ) {
-            // invalid cql -> null. other -> err.            
-            if( err.status !== 400 ) {
-                throw new Error( err );
-            }
-        }
-        return response;
-    }
+	  const req = Request.delete( apiUrl( cid, key ) );
+	  try {
+	    let response = await this.api.request( req );
+	    await this.api.request( req );
+	  } catch( err ) {            
+	    if( err.status == 404 ) {
+	      throw new Error( err );
+	    }
+	  }
+	}
+	/* ------------------------------------------------------------------------
+	 * 検索
+	 * https://docs.atlassian.com/ConfluenceServer/rest/6.15.7/#api/content-search
+	 * 更新日把握のため versionも取得する。もう少し拡張性のある書き方にしたい。
+	 * --------------------------------------------------------------------- */
+	private async restSearch( param: any = {} )  {
+	  const url = '/rest/api/content/search';
+	  const req = Request.get( url + queryToString( param ) );
+	  try {
+	    return await this.api.request( req );
+	  } catch( err ) {
+	    // invalid cql -> null. other -> err.            
+	    if( err.status !== 400 ) {
+	      throw new Error( err );
+	    }
+	  }
+	}
 	/* ------------------------------------------------------------------------
 	 * 全てのプロパティを取得する
 	 * --------------------------------------------------------------------- */
 	async getAll( cid: string, range: Range = new Range() ): Promise<ContentPropertyList> {
-		let response = await this.restFindAll( cid, { start: range.start, limit: range.limit } );
-		let list: ContentProperty[] = response.results.map( result => format( cid, result ) );
-		return {
-		    results: list,
-            range: new Range( {
-                start: response.start,
-                limit: response.limit,
-                size: response.size
-            } )
-		};
+	  let response = await this.restFindAll( cid, { start: range.start, limit: range.limit } );
+	  let list = response.results.map( result => format( cid, result ) );
+	  
+	  if( list == undefined ) {
+	    list = [];
+	  }
+	  
+	  return {
+	    results: list,
+	    range: rangeFromResponse( response )
+	  };
 	} 
 
-    /* ------------------------------------------------------------------------
-     * CQLに該当するプロパティを取得する
-     * Rangeは、Content Propertyが無いものも含んでいるため、結果の数とは合致しない。
-     * --------------------------------------------------------------------- */
-    async getByCql( cql: string, key: string, range: Range = new Range() ): Promise<ContentPropertyList> {
-        let response = await this.restSearch( {
-            cql: cql,
-            expand: 'version,metadata.properties.' + key,
-            start: range.start,
-            limit: range.limit
-        } );
-        
-        let list: ContentProperty[] = response.results
-                                              .filter( result => result.metadata.properties[ key ] !== undefined )
-                                              .map( result => format( result.id, result.metadata.properties[ key ] ) );
-        return {
-            results: list,
-            range: new Range( {
-                start: response.start,
-                limit: response.limit,
-                size: response.size
-            } )
-        };
-    } 
+  /** ---------------------------------------------------------------------
+   * @param cql CQL文字列。例えば 'SPACE=TEST'。
+   * @param key Content PropertyのKey
+   * @param range start:開始位置とlimit:最大データ数を指定する。sizeは無視する。
+   * @returns
+   * CQLにマッチするContent Propertyを取得する。
+   * Rangeは、start:開始数、size:取得データ数、limit:CQLにマッチしたContent数。
+   * limitにはContent Propertyが無いものも含まれる。
+   * --------------------------------------------------------------------- */
+	async getByCql( cql: string, key: string, range: Range = new Range() ): Promise<ContentPropertyList> {
+	  let response = await this.restSearch( {
+	    cql: cql,
+	    expand: 'version,metadata.properties.' + key,
+	    start: range.start,
+	    limit: range.limit
+	  } );
+	  
+	  let list: ContentProperty[] = response.results
+	  .filter( result => result.metadata.properties[ key ] != undefined )
+	  .map( result => format( result.id, result.metadata.properties[ key ] ) );
+	  
+	  return {
+	    results: list,
+	    range: rangeFromResponse( response )
+	  };
+	}
     
 	/* ------------------------------------------------------------------------
 	 * key の値を取得する。
@@ -221,12 +234,12 @@ export class ContentPropertyAPI {
         return format( cid, result );
 	}
 
-    /* ------------------------------------------------------------------------
-     * keyを削除する。
-     * 一括削除はAPIが無いため、負荷を抑えるために1つずつ削除する。
-     * --------------------------------------------------------------------- */
+	/* ------------------------------------------------------------------------
+	 * keyを削除する。
+	 * 一括削除はAPIが無いため、負荷を抑えるために1つずつ削除する。
+	 * --------------------------------------------------------------------- */
 	async delete( cid: string, key: string ) {
-	    await this.restDelete( cid, key );
+	  await this.restDelete( cid, key );
 	}
 }
 
