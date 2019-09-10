20533371d6c771bdc11e87966d968b78d8a773e6
stm32p103 <stm32p103@gmail.com>
2019-09-11 00:04:16 +0900

    drag and dropの練習(DOMイベントとの競合を考慮)

--- confluence/components/draggable-list.tsx
+++ confluence/components/draggable-list.tsx
@@ -1,13 +1,93 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+/* Confluence上でドラッグアンドドロップを実現するために、DOMイベントを利用する。 */
+
+const DATA_TYPE = 'text/my-data';
+
 export interface ListItem {
   key: string;
   label: string;
 }
 
-export const List = ( props: { items: ListItem[] } ) => <ul>{props.items.map( item => <ListItem item={item} /> ) }</ul>;
-export const ListItem = ( props: { item: ListItem } ) => <li key={props.item.key}>{props.item.label}</li>;
+interface Props {
+  items: ListItem[];
+}
+
+export class List extends React.Component<Props> {
+  constructor( props: Props ) {
+    super( props );
+  }
+  
+  render() {
+    return <div><Dropzone/>{this.props.items.map( item => <><DraggableItem item={item} /><Dropzone/></> ) }</div>;
+  }
+}
+
+export class Dropzone extends React.Component {
+  private element: React.RefObject<any>;
+  private readonly dropListener: any;
+  constructor( props: {} ) {
+    super( props );
+    this.element = React.createRef();
+    this.dropListener = this.dropped.bind( this );
+  }
+  
+  checkDrag( ev: React.DragEvent<HTMLDivElement> ) {
+    ev.preventDefault();
+  }
+  
+  checkEnter( ev: React.DragEvent<HTMLDivElement> ) {
+    ev.dataTransfer.dropEffect = 'move';
+    ev.preventDefault();
+  }
+  
+  componentDidMount() {
+    this.element.current.addEventListener( 'drop', this.dropListener );
+  }
+  
+  componentWillUnmount() {
+    this.element.current.removeEventListener( 'drop', this.dropListener );
+  }
+  
+  dropped( ev: React.DragEvent<HTMLDivElement> ) {
+    const data = ev.dataTransfer.getData( DATA_TYPE );
+    console.log( data );
+  }
+  
+  render() {
+    return <div ref={this.element}
+                onDragEnter={ ev => this.checkEnter( ev ) }
+                onDragOver={ ev => this.checkDrag( ev ) } >[drop]</div>
+  }
+}
+
+interface ListItemProps {
+  item: ListItem;
+}
+
+export class DraggableItem extends React.Component<ListItemProps> {
+  constructor( props: ListItemProps ) {
+    super( props );
+  }
+  
+  onDragStart( ev: React.DragEvent<HTMLDivElement> ) {
+    ev.dataTransfer.setData( DATA_TYPE, this.props.item.key );
+    ev.dataTransfer.effectAllowed = "copy";
+  }
+  
+  onDragEnd( ev: React.DragEvent<HTMLDivElement> ) {
+    console.log(ev.dataTransfer.dropEffect);
+  }
+  
+  render() { 
+    return <div draggable
+               key={this.props.item.key}
+               onDragStart ={ ev => this.onDragStart(ev) }
+               onDragEnd={ ev => this.onDragEnd(ev) }>{this.props.item.label}</div>;
+  }
+}
+
 
 export function testList( target: HTMLElement ) {
   const items: ListItem[] = [ 
