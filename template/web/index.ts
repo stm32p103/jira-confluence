f879772a516fa7fe61c76d595139366cfdc9f5e3
stm32p103 <stm32p103@gmail.com>
2019-09-20 03:22:13 +0900

    修正

--- template/web/index.ts
+++ template/web/index.ts
@@ -2,12 +2,13 @@ import { ContentPropertyAPI } from '@this/common';
 import { AJSRestAPI } from '@this/web';
 
 
-AJS.$(document).ready( () => {
+AJS.$(document).ready( async () => {
   const container = $('#app');
   const baseUrl = AJS.params.baseUrl;
   const cid = AJS.params.pageId;
   const api = new ContentPropertyAPI( new AJSRestAPI( baseUrl ) );
   
-  console.log( AJS.params );
+  const res = await api.getAll( cid );
+  console.log( res );
 } );
 
